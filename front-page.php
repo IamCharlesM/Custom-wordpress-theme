698e0704b17fa4f1c9232eb97ea9c9131e0cdf5f
Kittens <becauase@charlesis.me>
2019-03-27 11:01:23 -0500

    Bootstrap working, fixed typo

--- front-page.php
+++ front-page.php
@@ -1,4 +1,14 @@
 <?php get_header();?>
 
+<div class="container">
+    <div class="row">
+        <div class="col">
+            Left Side
+        </div>
+        <div class="col">
+        Right Side
+        </div>
+    </div>
+</div>
 
 <?php get_footer();?>
