d5b38b959374fb3e82ff34666a95948ca66c479d
Kittens <becauase@charlesis.me>
2019-03-30 11:46:56 -0500

    Header updated, title now shows up on the page.

--- front-page.php
+++ front-page.php
@@ -1,14 +1,8 @@
 <?php get_header();?>
 
-<div class="container">
-    <div class="row">
-        <div class="col">
-            Left Side
-        </div>
-        <div class="col">
-        Right Side
-        </div>
-    </div>
+<div class="container pt-5 pb-5">
+    <h1><?php the_title();?></h1>
+
 </div>
 
 <?php get_footer();?>
